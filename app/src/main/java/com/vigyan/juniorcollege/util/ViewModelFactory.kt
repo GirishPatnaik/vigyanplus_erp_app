dfec9be6e267be6e5a81bff86b67c3714d7cb9e0
GirishPatnaik <121415865+GirishPatnaik@users.noreply.github.com>
2026-09-05 20:35:38 +0530

    Update ViewModelFactory.kt

--- app/src/main/java/com/vigyan/juniorcollege/util/ViewModelFactory.kt
+++ app/src/main/java/com/vigyan/juniorcollege/util/ViewModelFactory.kt
@@ -2,6 +2,7 @@ package com.vigyan.juniorcollege.util
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewmodel.CreationExtras
+import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
 
 inline fun <reified VM : ViewModel> simpleFactory(crossinline create: () -> VM) =
